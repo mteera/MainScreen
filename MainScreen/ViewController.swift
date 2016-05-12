b53aa3a3ce2a8b5f4a224cae42a54aa22937cbc5
mteera <i7676893@mac27769.local>
2016-05-12 18:29:39 +0100

    initial commit

---
+++ MainScreen/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MainScreen
+//
+//  Created by Mongkol Teera (i7676893) on 10/05/2016.
+//  Copyright © 2016 Hexatech. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
